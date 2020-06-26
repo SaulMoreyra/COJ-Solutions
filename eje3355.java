c349a096b2eec9be17260ebdf0beec94bce97847
SaulMoreyra <armhacam21@gmail.com>
2020-06-26 11:03:20 -0500

    eje3355 eliminado

--- eje3355.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class eje3355
-{
-    public static void main(String [] args){
-        for(int i=0; i<3000; i++){
-            if(i===1000) System.out.println("MAIN CONTENT");
-        }
-    }
-}
